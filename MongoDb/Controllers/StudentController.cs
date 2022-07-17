8075ff4d32f97e76270e66d4389e0f2c95749b72
MrSEKIRO <ab.arshia@gmail.com>
2022-07-17 06:11:28 +0430

    implement Student controller

---
+++ MongoDb/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDb.Models;
+using MongoDb.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MongoDb.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StudentController : ControllerBase
+	{
+		private readonly IStudentService _studentService;
+
+		public StudentController(IStudentService studentService)
+		{
+			_studentService = studentService;
+		}
+		// GET: api/<StudentController>
+		[HttpGet]
+		public ActionResult<List<Student>> Get()
+		{
+			return _studentService.Get();
+		}
+
+		// GET api/<StudentController>/5
+		[HttpGet("{id}")]
+		public ActionResult<Student> Get(string id)
+		{
+			return _studentService.GetById(id);
+		}
+
+		// POST api/<StudentController>
+		[HttpPost]
+		public ActionResult Post([FromBody] Student student)
+		{
+			_studentService.Create(student);
+
+			return CreatedAtAction(nameof(Get), new { id = student.Id }, student);
+		}
+
+		// PUT api/<StudentController>/5
+		[HttpPut("{id}")]
+		public ActionResult Put(int id, [FromBody] Student student)
+		{
+			_studentService.Update(student);
+
+			return NoContent();
+		}
+
+		// DELETE api/<StudentController>/5
+		[HttpDelete("{id}")]
+		public ActionResult Delete(string id)
+		{
+			_studentService.Remove(id);
+
+			return Ok();
+		}
+	}
+}
--- MongoDb/Program.cs
+++ MongoDb/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.Extensions.Options;
+using MongoDb.DatabaseContext;
+using MongoDb.Services;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<StudentStoreDatabaseSettings>(
+	builder.Configuration.GetSection(nameof(StudentStoreDatabaseSettings)));
+
+builder.Services.AddSingleton<IStudentStoreDatabaseSettings>(s => s.GetRequiredService<IOptions<StudentStoreDatabaseSettings>>().Value);
+
+builder.Services.AddSingleton<IMongoClient>(s => new MongoClient(builder.Configuration.GetValue<string>("StudentStoreDatabaseSettings:ConnectionString")));
+
+builder.Services.AddScoped<IStudentService,StudentService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
